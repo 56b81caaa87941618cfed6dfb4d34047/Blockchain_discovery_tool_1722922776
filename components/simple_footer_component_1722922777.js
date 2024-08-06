f63d161e23ab7d457a0116053083c995d1343356
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-06 05:39:51 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722922777.js
+++ components/simple_footer_component_1722922777.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722922777", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BlockExplore - Discover Blockchain Data
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the curve with BlockExplore. Our decentralized app scours the blockchain to bring you the latest information and insights. Never miss an opportunity in the dynamic world of blockchain again.
                     </div>
                 </div>
             </div>
